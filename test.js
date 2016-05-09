d52418f362b089cd1bcb95e8c6670e03af1efef6
Nosthertus <nosthertus@gmail.com>
2016-05-09 02:44:48 -0430

    ADDED: test usage example
    *Added file of the package's usage as example

---
+++ test.js
@@ -0,0 +1,36 @@
+ var comment = {
+	header: "the header message",
+	example: {
+		traits: ["pageable", "searchable"]
+	},
+	params:[
+		{
+			type: "Object",
+			name: "obj",
+			description: "description of the object parameter"
+		},{
+			type: "String",
+			name: "str",
+			description: "description of the string parameter"
+		},{
+			return: true,
+			type: "Boolean",
+			description: "Wheter the value passes"
+		}
+	]
+};
+
+commmenter.buildBlock(comment);
+
+/**
+ * the header message
+ *
+ * example:
+ * 		*traits:
+ * 			- pageable
+ * 			- searchable
+ *
+ * @param  {Object} obj description of the object parameter
+ * @param  {String} str description of the string parameter
+ * @return {Boolean}    Wheter the value passes
+ */
